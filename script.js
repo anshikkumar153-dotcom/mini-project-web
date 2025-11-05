d19145b8678d340ae3c257767b63f3b9cee82ec2
anshikkumar153 <anshikkumar153@gmail.com>
2025-11-05 13:34:32 +0530

    project done

---
+++ script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+const bookButton = document.getElementById('book');
+
+let ticketPrice = +movieSelect.value;
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+});
+
+bookButton.addEventListener('click', () => {
+  const selectedSeats = document.querySelectorAll('.seat.selected');
+  if (selectedSeats.length === 0) {
+    alert('Please select at least one seat.');
+  } else {
+    alert(`🎟 Booking Confirmed!\nSeats: ${selectedSeats.length}\nTotal: ₹${total.innerText}`);
+  }
+});
